c859c3663aca13501b25c763042ccbda00939026
yosmods <146516623+yosmods@users.noreply.github.com>
2025-02-04 17:32:31 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,8 +1,8 @@
 {
-  "secretCode": "flean x yos",
+  "secretCode": "yosv6.0",
   "description": "PREMIUM KEY", 
   "lastUpdated": "20-01-2025",
-  "author": "FLEAN X YOS", 
+  "author": "YOS-OFFICIALL V6.0", 
   "status": "Active",
   "license": "MIT",
   "version": "3.0.0"
