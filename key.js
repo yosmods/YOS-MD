7ea1cd9301ba707217a89be4dabbd5dd154cc7f7
𝑀𝑅 : 𝑌𝑂𝑆-999 <146516623+yosmods@users.noreply.github.com>
2025-03-05 11:02:42 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,8 +1,8 @@
 {
-  "secretCode": "yosv6",
+  "secretCode": "YOS-OFFC",
   "description": "PREMIUM KEY", 
   "lastUpdated": "20-01-2025",
-  "author": "YOS-OFFICIALL V6.0", 
+  "author": "YOS-OFFICIALL", 
   "status": "Active",
   "license": "MIT",
   "version": "10.0.0"
