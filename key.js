b81830d81f1da38ccaf71e36a15e65e1ae7ed8ae
𝑀𝑅 : 𝑌𝑂𝑆-999 <146516623+yosmods@users.noreply.github.com>
2025-09-13 20:39:31 +0700

    Delete key.js

--- key.js
+++
@@ -1,9 +0,0 @@
-{
-  "secretCode": "YOS-999",
-  "description": "PREMIUM KEY", 
-  "lastUpdated": "2025-04-04",
-  "author": "YOS-999 ", 
-  "status": "Active",
-  "license": "MIT",
-  "version": "2.0.0"
-}
